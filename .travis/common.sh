163e7f42d4ac28ff2c97093cfdeb29315f9aeb56
tcely <tcely@users.noreply.github.com>
2019-01-01 12:11:21 -0500

    Update common.sh

--- .travis/common.sh
+++ .travis/common.sh
@@ -47,7 +47,7 @@ alpine_run() {
 	done >> "${ALPINE_ROOT}/.alpine_run_env"
 
 	$_sudo chroot "$ALPINE_ROOT" /usr/bin/env -i su -l $user \
-		sh -c "set -x ; . /.alpine_run_env ; cd \"\$CLONE_DIR\" ; set +x ; $cmd"
+		sh -c "set -x ; . /.alpine_run_env ; cd \"$CLONE_DIR\" ; set +x ; $cmd"
 }
 
 die() {
